66e8925abd1660dc6fd714d3828c3382246d01e7
Fenchel <msfenchel@gmail.com>
2012-12-16 11:29:41 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeDoDane::Application.config.secret_token = '6127baa4d2f1a20d3add303a168f02300d47d26871f56abf30def1af0abbfa85aad256f8383ac6989fcb134f31c043ceee54fa9e6a859b81561de5899457d1da'
